5fa22bacb90db439f1d3b22dcf2718988d41ba20
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-23 21:32:41 +0300

    Building the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />  
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <>
+        <h1>Far Away🌴🧳</h1>
+    </>
+  );
+}
+function Form() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="add-form">
+          <h3>What do you need for your trip?✈️</h3>
+        </div>
     </>
-  )
+  );
 }
+function PackingList() {
+  return (
+    <>
+        <div className="list">LIST</div>
+    </>
+  );
 
-export default App
+}
+function Stats() {
+
+  return (
+    <>
+        <footer className="stats">
+          <em>You have X items on your list, and you already packed X (X%)</em>
+        </footer>
+    </>
+  );
+}
