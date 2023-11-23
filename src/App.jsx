d18b027668b38f520213a18354eb37d048519ffc
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-23 23:17:24 +0300

    Building a form and handling submissions

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -28,12 +28,24 @@ function Logo() {
   );
 }
 function Form() {
+  function handleSubmit(e){
+    e.preventDefault();
+  }
 
   return (
     <>
-        <div className="add-form">
-          <h3>What do you need for your trip?✈️</h3>
-        </div>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <h3>What do you need for your trip?✈️</h3>
+
+        <select>
+          {Array.from( {length: 20} , (_, i) => i+1)
+          .map((num) => (
+          <option value={num} key={num}> {num} </option>))}
+        </select>
+
+        <input type='text' placeholder="Item..."></input>
+        <button>Add</button>
+      </form>
     </>
   );
 }
@@ -42,7 +54,7 @@ function PackingList() {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (<Item item={item} />))}
+        {initialItems.map((item) => (<Item item={item} key={item.id} />))}
       </ul>
     </div>
   );
@@ -62,9 +74,9 @@ function Item( {item} ){
 function Stats() {
   return (
     <>
-        <footer className="stats">
-          <em>You have X items on your list, and you already packed X (X%)</em>
-        </footer>
+      <footer className="stats">
+        <em>You have X items on your list, and you already packed X (X%)</em>
+      </footer>
     </>
   );
 }
