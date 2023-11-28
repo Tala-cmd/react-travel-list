05761957b647a77d2a4df2b2db4ac848646ed4a7
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-28 20:04:47 +0300

    Calculate statistics as derived state

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} 
       onToggleItem={handleToggleItem}/>
-      <Stats />  
+      <Stats items={items}/>  
     </div>
   );
 }
@@ -99,11 +99,29 @@ function Item( {item, onDeleteItem, onToggleItem} ){
 }
 
 
-function Stats() {
+function Stats( {items} ) {
+  if(!items.length){
+    return(
+      <p className="stats">
+        <em>Start adding some items to your packing list🚀</em>
+      </p>
+    )
+  }
+
+
+  const numItems = items.length
+  const numPacked = items.filter((item)=> item.packed).length
+  const percentage = Math.round((numPacked/numItems) * 100)
   return (
     <>
       <footer className="stats">
-        <em>You have X items on your list, and you already packed X (X%)</em>
+        <em>
+          {percentage === 100 ? 'You got everything! Ready to go✈️' :
+          `You have ${numItems} items on your list, 
+          and you already packed ${numPacked} (${percentage}%)` 
+          }
+          
+        </em>
       </footer>
     </>
   );
